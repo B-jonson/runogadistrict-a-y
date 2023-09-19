f0c615c48e71a5efb61006bde0a5c179cfb50e2b
B-jonson <141237855+B-jonson@users.noreply.github.com>
2023-09-19 05:30:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const slider = () => {
+    const slideRef = document.createElement( div );
+    const loadingProgress = 0;
+
+    const handleClickNext = ( )=> {
+        let items = slideRef.querySelectorAll(".item");
+        slideRef.append(items[0]);
+    };
+
+    const handleClickPrev = ( ) =>{
+        let items = slideRef.pretend(items[items.length - 1]);
+    }
+
+    const data = [
+        
+    ]
+
+const container = document.createElement("div");
+container.classList.add('container');
+
+
+document.getElementById("root").appendChild(container);
+
+}
+
+Slider();
